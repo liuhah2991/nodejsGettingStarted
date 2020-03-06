4e01469c855435781b282429e0b097f239fd2874
liuhah2991 <243006637@qq.com>
2020-03-06 16:36:06 +0800

    更名

--- FileBrower/main.js
+++ FileBrower/main.js
@@ -2,12 +2,13 @@ const http = require('http');
 const fs = require('fs');
 
 var server = http.createServer((req, res) => {
-    res.writeHeader(200, { 'Content-type': 'text/html', 'charset': 'utf-8' });
-    if (req.url == "/") {
-        fs.readFile("index.html", "utf-8", (err, file) => {
+    res.setHeader('Content-type', 'text/html;charset=utf-8');
+    if (req.url == '/') {
+        fs.readFile('index.html', 'utf-8', (err, file) => {
+            err && console.log(err);
             res.end(file);
         });
-    }
+    } 
 });
 
 server.listen(9000, () => {
